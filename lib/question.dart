3a468b639c4fe486240495ed1fbdd34b95080644
Alexandr Marchenko <alexandrm@rabota.ua>
2019-01-13 17:09:16 +0200

    switch to network image to reduce apk size

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class Answer {
   final String id;
@@ -120,11 +121,17 @@ class QuestionWidget extends StatelessWidget {
     ];
 
     if (question.image != null && question.image.indexOf('no_image') == -1) {
-      children.insert(
-          0,
-          ListTile(
-              title: Image.asset(
-                  'assets/images/' + question.image.split('/').last)));
+      // children.insert(0, ListTile(title: Image.asset('assets/images/' + question.image.split('/').last)));
+      String src = 'https://rawcdn.githack.com/mac2000/pdr/master/assets/images/' + question.image.split('/').last;
+      children.insert(0, ListTile(title: CachedNetworkImage(
+        imageUrl: src,
+        placeholder: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[new CircularProgressIndicator()],
+        ),
+        errorWidget: new Icon(Icons.error),
+      )));
     }
 
     return Scaffold(
